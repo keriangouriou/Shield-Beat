aef777441dd51dfa26631811b0b7a5f7c16f4321
Kerian <kerian.gouriou@gmail.com>
2026-04-23 19:45:53 +0200

    Added 3D models and work on color and Level Design
    I forgot to commit for a bit too long so there is more thing in this commit that I would want to. My bad.

--- Shield Beat/Assets/Scripts/GameManagerScript.cs
+++ Shield Beat/Assets/Scripts/GameManagerScript.cs
@@ -3,7 +3,13 @@ using UnityEngine;
 public class GameManagerScript : MonoBehaviour
 {
     [SerializeField]
-    private float currentEnergy = 100;
+    private float currentEnergy = 90;
+    [SerializeField]
+    private Material materialEnergy, materialEnemy;
+    private int energyState = 5;
+    private Color materialEnergyColor = new Color(0,1,0);
+    private Color materialEnemyColor = new Color(1,0,1);
+    private float intensity;
     void Start()
     {
         
@@ -15,6 +21,22 @@ public class GameManagerScript : MonoBehaviour
         if (currentEnergy <= 0 )
         {
             Lose();
+        }
+
+        switch (currentEnergy)
+        {
+            case <50:
+                EnergyVeryLow();
+                break;
+            case < 100:
+                EnergyLow();
+                break;
+            case < 150:
+                EnergyHigh();
+                break;
+            case >= 150:
+                EnergyVeryHigh();
+                break;
         }
     }
     public void GainEnergy(float energyAdded)
@@ -28,5 +50,39 @@ public class GameManagerScript : MonoBehaviour
     private void Lose()
     {
         Debug.Log("You Lose");
+    }
+
+    private void EnergyVeryLow()
+    {
+        if (energyState != 1)
+        {
+            intensity = -2;
+            materialEnergy.SetColor("_EmissionColor", materialEnergyColor*0f);
+            energyState = 1;
+        }
+    }
+    private void EnergyLow()
+    {
+        if (energyState != 2)
+        {
+            materialEnergy.SetColor("_EmissionColor", materialEnergyColor*0.2f);
+            energyState = 2;
+        }
+    }
+    private void EnergyHigh()
+    {
+        if (energyState != 3)
+        {
+            materialEnergy.SetColor("_EmissionColor", materialEnergyColor*0.7f);
+            energyState = 3;
+        }
+    }
+    private void EnergyVeryHigh()
+    {
+        if (energyState != 4)
+        {
+            materialEnergy.SetColor("_EmissionColor", materialEnergyColor * 1.2f);
+            energyState = 4;
+        }
     }
 }
--- Shield Beat/Assets/Scripts/ProjectileScript.cs
+++ Shield Beat/Assets/Scripts/ProjectileScript.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 public class ProjectileScript : MonoBehaviour
 {
     [SerializeField]
-    private float speed;
+    private float speed, beat;
+    private float beatDistance = 9.2308f; //The result of Time in second between beats * speed
     public float value;
+    private void Start()
+    {
+        transform.Translate(0,0, beatDistance * (beat-1));
+    }
     private void Update()
     {
             transform.Translate(0,0,-speed*Time.deltaTime);
